578c411129646a58d1cacc6cc6e9e3856279572a
CsavRR <fleisz.orbi@gmail.com>
2024-03-21 19:28:40 +0100

    2. feladat

--- ugyfelszolgalat.py
+++ ugyfelszolgalat.py
@@ -1,2 +1,10 @@
 def mbpe(o, p, m):
     return o * 60 * 60 + p * 60 + m
+
+hivas = []
+f = open('hivas.txt', 'rt')
+
+for sor in f:
+    sor = sor.strip().split(' ')
+    sor = list(map(int, sor))
+    hivas.append(sor)
