4a7031b03a0652a70a3249fd620b3831293a6a1a
CsavRR <fleisz.orbi@gmail.com>
2024-03-21 20:13:00 +0100

    3. feladat

--- ugyfelszolgalat.py
+++ ugyfelszolgalat.py
@@ -8,3 +8,14 @@ for sor in f:
     sor = sor.strip().split(' ')
     sor = list(map(int, sor))
     hivas.append(sor)
+
+print('3. feladat')
+orak = {}
+
+for ora in hivas:
+    if ora[0] not in orak.keys():
+        orak[ora[0]] = 0
+    orak[ora[0]] += 1
+
+for k, v in orak.items():
+    print(f'{k} ora {v} hivas')
